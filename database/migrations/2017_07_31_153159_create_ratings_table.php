56ad914ebc16248d604ed858190bd3f6a85623d6
singularitarian <nsbertanzetti@crimson.ua.edu>
2017-08-01 21:32:47 -0500

    database migrations, create user

--- database/migrations/2017_07_31_153159_create_ratings_table.php
+++ database/migrations/2017_07_31_153159_create_ratings_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRatingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('ratings', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('score');
+            $table->string('user_id')->foreign('user_id')->references('id')->on('users')->nullable();
+            $table->string('route_id')->foreign('route_id')->references('id')->on('routes')->nullable();
             $table->timestamps();
         });
+
+
     }
 
     /**
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        //
     }
 }
